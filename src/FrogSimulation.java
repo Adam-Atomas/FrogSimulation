66c451d3c3cab6d0b04c5cce0f700634fcd86229
AdamShi <adams221@nycstudents.net>
2025-11-14 12:34:13 -0500

    start

---
+++ src/FrogSimulation.java
@@ -0,0 +1,16 @@
+public class FrogSimulation {
+    private int goalDistance;
+    private int maxHops;
+    public FrogSimulation(int dist, int numHops){
+        if (dist>0 && numHops>0) {
+            goalDistance = dist;
+            maxHops = numHops;
+        }
+    }
+    private int hopDistance(){
+    return goalDistance
+    }
+    public boolean simulate(){
+
+    }
+}
