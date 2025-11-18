571626ffd14940ad400fa096f6a431045be8cdc5
AdamShi <adams221@nycstudents.net>
2025-11-18 12:34:24 -0500

    start

--- src/FrogSimulation.java
+++ src/FrogSimulation.java
@@ -8,9 +8,27 @@ public class FrogSimulation {
         }
     }
     private int hopDistance(){
-    return goalDistance
+        int sign;
+        if (Math.random()>=0.5){
+            sign= -1;
+        }
+        else{
+            sign = 1;
+        }
+    return (sign)*(int)(Math.random()*(goalDistance+1));
     }
     public boolean simulate(){
-
+        int FinalPos=0;
+    for (int i=1; i<=maxHops; i++){
+        int HopDist = hopDistance();
+        FinalPos+= HopDist;
+        System.out.print(HopDist+" ");
+        if (FinalPos>=goalDistance){
+            System.out.print("Final:"+FinalPos+" ");
+            return true;
+        }
+    }
+    System.out.print("Final:"+FinalPos+" ");
+    return false;
     }
 }
